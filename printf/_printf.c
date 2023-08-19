1b04b0c1c68fc2d398ef800b294bfb25bd928829
LeoTheNrdKng <lionheljoela@gmail.com>
2023-08-19 19:32:53 +0200

    Updated .c file with new lines of code.

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,102 +1,104 @@
 #include "main.h"
-/**
-  *_printf - function that prints a formatted output.
-  *@format: format string containing characters & specifiers
-  *Return: The number of characters printed
-  */
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success 1. On error, -1 is returned and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
+/**
+ * print_c - prints a character
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_c(va_list args)
 {
 	char c = va_arg(args, int);
 	_putchar(c);
-	return 1;
+	return (1);
 }
 
-int print_percent(va_list args)
-{
-	_putchar('%');
-	return 1;
-}
-
-int print_i(va_list args)
-{
-	return write(1, &c, 1);
-}
-
+/**
+ * print_s - prints a string
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_s(va_list args)
 {
 	char *str = va_arg(args, char *);
 	int length = 0;
 
-	if (str != NULL) 
+	if (str != NULL)
 	{
 		while (str[length])
+		{
 			_putchar(str[length]);
-		length++;
+			length++;
+		}
 	}
+	return (length);
 }
 
-int print_d(va_list args)
+/**
+ * print_percent - prints a percent symbol
+ * @args: A list of variadic arguments (unused)
+ * Return: Number of characters printed
+ */
+int print_percent(va_list args)
 {
-	int num = va_arg(args, int);
-	char buffer[20];
-	int length = sprintf(buffer, "%d", num);
-	int i;
-
-	for (i = 0; i < length; i++)
-{
-	_putchar(buffer[i]);
+	(void)args;
+	_putchar('%');
+	return (1);
 }
 
-return length;
-}
-
+/**
+ * _printf - prints a formatted string
+ * @format: The format string
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	va_start(args, format);
 
+	/* Declare the variable `printed_chars` before the loop. */
 	int printed_chars = 0;
-}
 
-while (*format)
-{
-	if (*format == '%')
+	int i = 0;
+	while (format[i])
 	{
-		format++;
-
-		if (*format == 'c') 
+		if (format[i] == '%')
 		{
-			printed_chars += print_c(args);
-		}
-		else if (*format == '%')
-		{
-			printed_chars =+ print_percent(args);
-		}
-		else if (*format == 'd')
-		{
-			printed_chars += print_d(args);
-		}
-		else if (8format == 's')
-		{
-			printed_chars += print_s(args);
+			i++;
+			if (format[i] == 'c')
+				printed_chars += print_c(args);
+			else if (format[i] == 's')
+				printed_chars += print_s(args);
+			else if (format[i] == '%')
+				printed_chars += print_percent(args);
+			else
+				{
+					_putchar('%');
+					_putchar(format[i]);
+					printed_chars += 2;
+				}
 		}
 		else
 		{
-			_putchar(*format);
+			_putchar(format[i]);
 			printed_chars++;
 		}
-	}
-	else
-	{
-		_putchar(*format);
-		printed_chars++;
+		i++;
 	}
 
-	format++;
+	va_end(args);
+	return (printed_chars);
 }
--- printf/main.h
+++ printf/main.h
@@ -1,23 +1,58 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-void print_f(va_list element);
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success 1, and -1 is returned on error.
+ */
+int _putchar(char c);
 
 /**
- * _printf: Prototype function for printf
- *
+ * print_c - prints a character
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
  */
-
-int _printf(const char *format, ...);
 int print_c(va_list args);
+
+/**
+ * print_s - prints a string
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_s(va_list args);
+
+/**
+ * print_percent - prints a percent symbol
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_percent(va_list args);
+
+/**
+ * print_d - prints an integer
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_d(va_list args);
+
+/**
+ * print_i - prints an integer
+ * @args: A list of variadic arguments
+ * Return: Number of characters printed
+ */
 int print_i(va_list args);
-int _putchar(char c); 
+
+/**
+ * _printf - prints a formatted string
+ * @format: The format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...);
 
 #endif /* _MAIN_H_ */
