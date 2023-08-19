629416cf2739dd4c368070608833af60977e1196
LeoTheNrdKng <lionheljoela@gmail.com>
2023-08-19 23:19:35 +0200

    Made changes in the scripts.

--- printf.c
+++ printf.c
@@ -5,60 +5,60 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int num;
-    unsigned int u_num;
-    char buffer[20];
-    char u_buffer[20];
+va_list args;
+int count = 0;
+int num;
+unsigned int u_num;
+char buffer[20];
+char u_buffer[20];
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    break;
-                case 's':
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                case 'd':
-                case 'i':
-                    num = va_arg(args, int);
-                    {
-                        int len = snprintf(buffer, sizeof(buffer), "%d", num);
-                        write(1, buffer, len);
-                        count += len;
-                    }
-                    break;
-                case 'u':
-                    u_num = va_arg(args, unsigned int);
-                    {
-                        int u_len = snprintf(u_buffer, sizeof(u_buffer), "%u", u_num);
-                        write(1, u_buffer, u_len);
-                        count += u_len;
-                    }
-                    break;
-                default:
-                    write(1, format - 1, 2);
-                    count += 2;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+break;
+case 's':
+break;
+case '%':
+write(1, "%", 1);
+count++;
+break;
+case 'd':
+case 'i':
+num = va_arg(args, int);
+{
+int len = snprintf(buffer, sizeof(buffer), "%d", num);
+write(1, buffer, len);
+count += len;
+}
+break;
+case 'u':
+u_num = va_arg(args, unsigned int);
+{
+int u_len = snprintf(u_buffer, sizeof(u_buffer), "%u", u_num);
+write(1, u_buffer, u_len);
+count += u_len;
+}
+break;
+default:
+write(1, format - 1, 2);
+count += 2;
+}
+}
+else
+{
+write(1, format, 1);
+count++;
+}
+format++;
+}
 
-    va_end(args);
-    return count;
+va_end(args);
+return (count);
 }
