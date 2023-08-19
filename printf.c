d4ef222b2bffbc44c6755869f0ed3b12d5864787
LeoTheNrdKng <lionheljoela@gmail.com>
2023-08-19 21:34:25 +0200

    Made a few more changes.

--- printf.c
+++ printf.c
@@ -1,75 +1,52 @@
 #include "main.h"
 #include <unistd.h>
+#include <limits.h>
 
-/**
- * _putchar - Writes a character to standard output.
- * @c: The character to be written.
- * Return: On success, the number of characters written.
- */
-int _putchar(char c)
-{
+int _putchar(char c) {
 return (write(1, &c, 1));
 }
 
-/**
- * _printf - Produces output according to a format.
- * @format: Format string containing characters and specifiers.
- *
- * Description:
- * This function processes the format string and calls appropriate printing
- * functions based on the conversion specifiers. It also handles flags for
- * formatting options.
- *
- * Return:
- * The total number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-const char *p;
-va_list arguments;
-
-register int count = 0;
-
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-{
-va_end(arguments);
-return (-1);
-}
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-{
-va_end(arguments);
-return (-1);
-}
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == 'c')
-{
+static void handle_conversion(va_list arguments, int *count, char specifier) {
+if (specifier == 'c') {
 char ch = (char)va_arg(arguments, int);
-count += _putchar(ch);
-continue;
-}
-else if (*p == 's')
-{
+*count += _putchar(ch);
+} else if (specifier == 's') {
 char *str = va_arg(arguments, char *);
-while (*str)
-{
-count += _putchar(*str);
+while (*str) {
+*count += _putchar(*str);
 str++;
 }
-continue;
-}
-else if (*p == '%')
-{
-count += _putchar('%');
-continue;
+} else if (specifier == '%') {
+*count += _putchar('%');
 }
 }
+
+int _printf(const char *format, ...) {
+const char *p;
+va_list arguments;
+int count = 0;
+
+va_start(arguments, format);
+
+if (!format || (format[0] == '%' && !format[1])) {
+va_end(arguments);
+return (-1);
+}
+if (format[0] == '%' && format[1] == ' ' && !format[2]) {
+va_end(arguments);
+return (-1);
+}
+
+for (p = format; *p; p++) {
+if (*p == '%' && *(p + 1)) {
+p++;
+handle_conversion(arguments, &count, *p);
+} else {
 count += _putchar(*p);
 }
+}
+
+_putchar(-1);
 va_end(arguments);
 return (count);
 }
