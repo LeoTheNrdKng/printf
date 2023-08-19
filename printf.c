d9bed74cb98d7d9cc3f9421180e3f7a82ce7fb68
LeoTheNrdKng <lionheljoela@gmail.com>
2023-08-19 21:57:32 +0200

    made more changes.

--- printf.c
+++ printf.c
@@ -1,41 +1,36 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * _putchar - Writes a character to standard output.
+ * @c: The character to be written.
+ * Return: On success, the number of characters written.
+ */
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
 
-static void handle_conversion(va_list arguments, int *count, char specifier)
-{
-if (specifier == 'c')
-{
-char ch = (char)va_arg(arguments, int);
-*count += _putchar(ch);
-}
-else if (specifier == 's')
-{
-char *str = va_arg(arguments, char *);
-while (*str)
-{
-*count += _putchar(*str);
-str++;
-}
-}
-else if (specifier == '%')
-{
-*count += _putchar('%');
-}
-}
-
+/**
+ * _printf - Produces output according to a format.
+ * @format: Format string containing characters and specifiers.
+ *
+ * Description:
+ * This function processes the format string and calls appropriate printing
+ * functions based on the conversion specifiers. It also handles flags for
+ * formatting options.
+ *
+ * Return:
+ * The total number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
 const char *p;
 va_list arguments;
-int count = 0;
+
+register int count = 0;
 
 va_start(arguments, format);
-
 if (!format || (format[0] == '%' && !format[1]))
 {
 va_end(arguments);
@@ -46,21 +41,35 @@ if (format[0] == '%' && format[1] == ' ' && !format[2])
 va_end(arguments);
 return (-1);
 }
-
 for (p = format; *p; p++)
 {
-if (*p == '%' && *(p + 1))
+if (*p == '%')
 {
 p++;
-handle_conversion(arguments, &count, *p);
-}
-else
+if (*p == 'c')
 {
+char ch = (char)va_arg(arguments, int);
+count += _putchar(ch);
+continue;
+}
+else if (*p == 's')
+{
+char *str = va_arg(arguments, char *);
+while (*str)
+{
+count += _putchar(*str);
+str++;
+}
+continue;
+}
+else if (*p == '%')
+{
+count += _putchar('%');
+continue;
+}
+}
 count += _putchar(*p);
 }
-}
-
-_putchar(-1);
 va_end(arguments);
 return (count);
 }
