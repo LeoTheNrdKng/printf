a3e963671daa4a9c951db71334e0c25a19353223
ShivaliSewcharran <shivalisewcharran@gmail.com>
2023-08-22 02:48:02 -0700

    man page

--- over_test/test/printf.c
+++ over_test/test/printf.c
@@ -1,16 +1,31 @@
 #include "main.h"
 
 /**
- * _printf - Custom implementation of printf function.
- * @format: The format string containing format specifiers.
- * @...: Additional arguments based on format specifiers.
+ * _putchar - Writes a character to stdout.
+ * @c: The character to write.
  *
- * Return: Number of characters printed.
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * _printf - Custom implementation of the printf function.
+ * @format: The format string.
+ * @...: Additional arguments depending on the format string.
+ *
+ * Return: The total number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
+int num;
+unsigned int u_num;
+char buffer[20];
+char u_buffer[20];
 
 va_start(args, format);
 
@@ -19,7 +34,38 @@ while (*format)
 if (*format == '%')
 {
 format++;
-process_format(format, args, &count);
+switch (*format)
+{
+case 'c':
+handle_char(args, &count);
+break;
+case 's':
+handle_string(args, &count);
+break;
+case '%':
+handle_percent(&count);
+break;
+case 'd':
+case 'i':
+num = va_arg(args, int);
+{
+int len = snprintf(buffer, sizeof(buffer), "%d", num);
+write(1, buffer, len);
+count += len;
+}
+break;
+case 'u':
+u_num = va_arg(args, unsigned int);
+{
+int u_len = snprintf(u_buffer, sizeof(u_buffer), "%u", u_num);
+write(1, u_buffer, u_len);
+count += u_len;
+}
+break;
+default:
+write(1, format - 1, 2);
+count += 2;
+}
 }
 else
 {
